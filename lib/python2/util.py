b2b6a9f9cc5bc4f5eb145c3c561561ed8a2fb8e1
jjjj222 <jjjj222.tso@gmail.com>
2017-12-19 01:30:43 -0800

    Backup

--- lib/python2/util.py
+++ lib/python2/util.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import time
+from StringIO import StringIO
 
 #-------------------------------------------------------------------------------
 #   
@@ -80,6 +81,48 @@ def remove_file(filename):
         pass
 
 #-------------------------------------------------------------------------------
+#   tokenize
+#-------------------------------------------------------------------------------
+# TODO: support double quote (")
+def tokenize_tcl_list(tcl_list):
+
+    iter = enumerate(tcl_list)
+
+    result = tokenize_tcl_list_helper(iter)
+
+    return result
+
+
+def tokenize_tcl_list_helper(iterator):
+    result = []
+    try:
+        sstm = StringIO()
+        while True:
+            i, c = next(iterator)
+            if c == "{":
+                nested_result = tokenize_tcl_list_helper(iterator)
+                result.append(nested_result)
+            elif c == "}":
+                break
+            elif c == " ":
+                token = sstm.getvalue()
+                sstm = StringIO()
+                if len(token) != 0:
+                    result.append(token)
+            else:
+                sstm.write(c)
+
+    except StopIteration:
+        pass
+
+    token = sstm.getvalue()
+    sstm = StringIO()
+    if len(token) != 0:
+        result.append(token)
+
+    return result
+
+#-------------------------------------------------------------------------------
 #   assert
 #-------------------------------------------------------------------------------
 def assert_and_get_env(name):
@@ -100,3 +143,11 @@ def assert_isdir(dirname):
     if not os.path.isdir(dirname):
         raise AssertionError("'%s' is not a directory !!" % dirname)
 
+#-------------------------------------------------------------------------------
+#   
+#-------------------------------------------------------------------------------
+def dump_dict(d):
+    print "{"
+    for key, value in d.iteritems():
+        print "    \"%s\": \"%s\"," % (key, value)
+    print "}"
